43d01cfee0996b77f430559b09fee03c43220e9b
Pablo <pablito@me.com>
2023-09-11 14:00:03 +0100

    baseUrl.js no longer ignored by git

---
+++ frontend/public/baseUrl.js
@@ -0,0 +1,7 @@
+// module.exports = "http://localhost:4000"; // local development
+
+// module.exports = "https://alternative-routes-backend.onrender.com"; // render deployment
+
+// export const baseUrl = "http://localhost:4000"; // local development
+
+export const baseUrl = "https://alternative-routes-backend.onrender.com"; // render deployment
