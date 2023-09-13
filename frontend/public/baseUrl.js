cd254374d46bb5c9e490ea0e754d6e1b2df0a09a
Pablo <pablito@me.com>
2023-09-13 15:46:15 +0100

    Set baseUrl to local dev URL

--- frontend/public/baseUrl.js
+++ frontend/public/baseUrl.js
@@ -2,6 +2,6 @@
 
 // module.exports = "https://alternative-routes-backend.onrender.com"; // render deployment
 
-// export const baseUrl = "http://localhost:4000"; // local development
+export const baseUrl = "http://localhost:4000"; // local development
 
-export const baseUrl = "https://alternative-routes-backend.onrender.com"; // render deployment
+// export const baseUrl = "https://alternative-routes-backend.onrender.com"; // render deployment
