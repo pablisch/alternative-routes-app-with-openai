9a913035ccaf64761c25d9fd06fd5d4f414e2389
Pablo <pablito@me.com>
2023-09-13 15:47:14 +0100

    Added messageModel for storing messages in DB

---
+++ backend/models/messageModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    // maxlength: 500
+  },
+  subcribe: {
+    type: Boolean,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
