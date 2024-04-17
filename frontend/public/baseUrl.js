fa34f66f8a1cb720c4bcaa7897156d90591facba
Pablo <pablito@me.com>
2024-04-17 20:03:00 +0100

    Brought in responsive changes and reverted baseUrl to deployed backend.

--- frontend/public/baseUrl.js
+++ frontend/public/baseUrl.js
@@ -4,6 +4,6 @@
 
 // export const baseUrl = "http://localhost:4000"; // local development
 
-export const baseUrl = process.env.BASEURL || "http://localhost:4000"; // render deployment OR local development
+// export const baseUrl = process.env.BASEURL || "http://localhost:4000"; // render deployment OR local development
 
-// export const baseUrl = "https://alternative-routes-backend.onrender.com"; // render deployment
+export const baseUrl = "https://alternative-routes-backend.onrender.com"; // render deployment
